19b734d52600d0c6554b161ca25fe3ef14c992a4
arifin <arifin.nurhidayah1933@gmail.com>
2022-02-14 11:43:27 +0700

    + implement layout ProfileFragment + implement layout Dashboard

--- app/src/main/java/com/arifin/daringschool/MainActivity.java
+++ app/src/main/java/com/arifin/daringschool/MainActivity.java
@@ -1,14 +1,84 @@
 package com.arifin.daringschool;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.arifin.daringschool.Fragment.HomeFragment;
+import com.arifin.daringschool.Fragment.ProfileFragment;
+import com.arifin.daringschool.Fragment.TodoFragment;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.nav_view);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_note));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_account));
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment = null;
+
+                switch (item.getId()){
+                    case 1:
+                        fragment = new TodoFragment();
+                        break;
+                    case 2:
+                        fragment = new HomeFragment();
+                        break;
+                    case 3:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+
+//        bottomNavigation.setCount(1, "10");
+//
+        bottomNavigation.show(2, true);
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText(getApplicationContext()
+//                , "You Clicked " + item.getId()
+//                , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText(getApplicationContext()
+//                        , "You Reselected " + item.getId()
+//                        , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit();
     }
 }
